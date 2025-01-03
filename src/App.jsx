79a2eb32d9b104473b09cf5b00c5fbd791ee798b
Roy Mosby <ego.madking@gmail.com>
2025-01-02 19:27:21 -0500

    pagination implemented with cache disabled

--- src/App.jsx
+++ src/App.jsx
@@ -9,16 +9,24 @@ function App() {
   const [term, setTerm] = useState('');
   const [recipes, setRecipes] = useState([]);
   const [searchCache, setSearchCache] = useState({});
+  const [currentOffset, setCurrentOffset] = useState(0);
+  const [nextOffset, setNextOffset] = useState(0);
+  const [resultsCount, setResultsCount] = useState(0);
+  const [isPaginationDisabled, setIsPaginationDisabled] = useState(false);
+  const paginationSize = 10;
 
   useEffect(() => {
     if (!term) {
       return;
     }
-    if (searchCache[term]) {
-      setRecipes([...searchCache[term]]);
-      setTerm('');
-      return;
-    }
+    const pendingQuery = `${term} offset ${nextOffset}`;
+    console.log(pendingQuery);
+    // if (searchCache[pendingQuery]) {
+    //   setCurrentOffset(nextOffset);
+    //   setRecipes([...searchCache[term]]);
+    //   setTerm('');
+    //   return;
+    // }
     async function getRecipes() {
       const options = {
         headers: {
@@ -26,33 +34,95 @@ function App() {
           'x-api-key': `${KEY}`,
         },
       };
+      const params = [
+        `includeIngredients=${term}`,
+        `addRecipeInformation=true`,
+        `offset=${nextOffset}`,
+        `number=${paginationSize}`,
+      ];
+      const url = `${BASE_URL}/complexSearch?${params.join('&')}`;
+      console.log(url);
       try {
-        const resp = await fetch(
-          `${BASE_URL}/complexSearch?includeIngredients=${term}&addRecipeInformation=true`,
-          options
-        );
+        const resp = await fetch(url, options);
         if (resp.ok) {
-          // resp includes number, offset, totalResults
-          const recipeList = await resp.json();
-          setRecipes([...recipeList.results]);
-          setSearchCache((prev) => ({
-            ...prev,
-            [term]: [...recipeList.results],
-          }));
-          setTerm('');
+          const results = await resp.json();
+          setRecipes([...results.results]);
+          setResultsCount(results.totalResults);
+          setCurrentOffset(results.offset);
+          // setSearchCache((prev) => {
+          //   console.log('debug setSearchCache', prev);
+          //   console.dir({
+          //     ...prev,
+          //     [`${term} offset ${results.offset}`]: [...results.results],
+          //   });
+          //   return {
+          //     ...prev,
+          //     [`${term} offset ${results.offset}`]: [...results.results],
+          //   };
+          // });
+          //setTerm('');
         }
       } catch (e) {
         console.log(e);
       }
     }
     getRecipes();
-  }, [term, searchCache]);
+  }, [term, searchCache, nextOffset]);
+
+  useEffect(() => {
+    setNextOffset(0);
+  }, [term]);
+
+  function pageForward() {
+    setIsPaginationDisabled(true);
+    const maxPages = Math.ceil(resultsCount / paginationSize);
+    const currentPage = Math.ceil(currentOffset / paginationSize) + 1;
+    if (currentPage >= maxPages) {
+      return;
+    }
+    setNextOffset(currentOffset + paginationSize);
+    setTimeout(() => setIsPaginationDisabled(false), 1000);
+  }
+
+  function pageBack() {
+    setIsPaginationDisabled(true);
+    if (currentOffset <= 0) {
+      return;
+    }
+    setNextOffset(currentOffset - paginationSize);
+    setTimeout(() => setIsPaginationDisabled(false), 1000);
+  }
+
+  function isBackButtonDisabled() {
+    return currentOffset + paginationSize >= resultsCount;
+  }
+
   return (
     <>
       <main>
         <h1>Find Recipes by Ingredient</h1>
         <IngredientForm term={term} setTerm={setTerm} />
         <RecipeList results={recipes} />
+        {recipes.length > 0 && (
+          <div className="paginate">
+            <button
+              disabled={currentOffset === 0 || isPaginationDisabled}
+              onClick={pageBack}
+            >
+              Previous
+            </button>
+            <span>
+              Page {Math.floor(currentOffset / paginationSize) + 1} of{' '}
+              {Math.ceil(resultsCount / paginationSize)}
+            </span>
+            <button
+              onClick={pageForward}
+              disabled={isBackButtonDisabled() || isPaginationDisabled}
+            >
+              Next
+            </button>
+          </div>
+        )}
       </main>
     </>
   );
